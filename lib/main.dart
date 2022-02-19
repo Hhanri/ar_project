f99026e020a1dbace46b0a7b552968223edcb04a
Hanri <henry018@laposte.net>
2022-02-19 14:59:47 +0100

    spawns red ball on plane

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
 import 'package:flutter/material.dart';
+import 'package:vector_math/vector_math_64.dart' as vector;
 
 void main() {
   runApp(const MyApp());
@@ -24,11 +26,46 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late ArCoreController arCoreController;
+  ArCoreNode? node;
 
   @override
+  void dispose() {
+    super.dispose();
+    arCoreController.dispose();
+  }
+
+  void _onArCoreViewCreated(ArCoreController controller) {
+    arCoreController = controller;
+    arCoreController.onPlaneDetected = _handleOnPlaneDetected;
+  }
+
+  _handleOnPlaneDetected(ArCorePlane plane) {
+    if (node != null ){
+      arCoreController.removeNode(nodeName: node!.name);
+    }
+    _addSphere(arCoreController, plane);
+  }
+  _addSphere(ArCoreController controller, ArCorePlane plane) {
+    final ArCoreMaterial material = ArCoreMaterial(color:  Colors.red);
+    final ArCoreSphere sphere = ArCoreSphere(materials: [material], radius: 0.1);
+    node = ArCoreNode(
+      name: 'sphere',
+      shape: sphere,
+      position: plane.centerPose?.translation,
+      rotation: plane.centerPose?.rotation
+    );
+    controller.addArCoreNode(node!);
+  }
+  @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       body: Center(
+        child: ArCoreView(
+          onArCoreViewCreated: _onArCoreViewCreated,
+          enableUpdateListener: true,
+        ),
       ),
     );
   }
